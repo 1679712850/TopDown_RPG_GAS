1ef5ed6c81757e5187f16aa2fbe1d7afa64306fe
xiaofengzi <1679712850@qq.com>
2025-12-02 16:13:59 +0900

    Base Character Class

---
+++ Source/TopDown_RPG_GAS/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,21 @@
+// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class TOPDOWN_RPG_GAS_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+};

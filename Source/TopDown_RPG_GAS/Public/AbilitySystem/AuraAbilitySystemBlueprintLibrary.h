afd10c341c4e1b1a85f3e72f91bd3008c517e568
xiaofengzi <1679712850@qq.com>
2025-12-11 14:58:19 +0900

    Aura Ability System Blueprint Library

---
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/AuraAbilitySystemBlueprintLibrary.h
@@ -0,0 +1,21 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemBlueprintLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UAuraAbilitySystemBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};

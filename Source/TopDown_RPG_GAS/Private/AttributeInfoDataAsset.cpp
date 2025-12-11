29edea7ca98ae8d77ad6eeb14ab8f664b3771b6f
xiaofengzi <1679712850@qq.com>
2025-12-11 14:33:46 +0900

    Attribute Info Data Asset

---
+++ Source/TopDown_RPG_GAS/Private/AttributeInfoDataAsset.cpp
@@ -0,0 +1,23 @@
+﻿// Copyright 13
+
+
+#include "AttributeInfoDataAsset.h"
+
+FAuraAttributeInfo UAttributeInfoDataAsset::FindAttributeInfoForTag(const FGameplayTag& AttributeTag,
+                                                                    bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(),*GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
---
+++ Source/TopDown_RPG_GAS/Public/AttributeInfoDataAsset.h
@@ -0,0 +1,41 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfoDataAsset.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag AttributeTag = FGameplayTag();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeName = FText();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UAttributeInfoDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	FAuraAttributeInfo FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraAttributeInfo> AttributeInformation;
+};

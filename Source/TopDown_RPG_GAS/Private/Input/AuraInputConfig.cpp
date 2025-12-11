f81c8afd5e380e0af4158740039c6ccf1e0e9d75
xiaofengzi <1679712850@qq.com>
2025-12-11 21:26:20 +0900

    Input Config Data Asset

---
+++ Source/TopDown_RPG_GAS/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright 13
+
+
+#include "AuraInputConfig.h"
+
+UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound)
+{
+	for (const auto& AbilityInputAction : AbilityInputActions)
+	{
+		if (AbilityInputAction.InputAction && AbilityInputAction.InputTag == InputTag)
+		{
+			return AbilityInputAction.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s], on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this));
+	}
+	
+	return nullptr;
+}
---
+++ Source/TopDown_RPG_GAS/Public/Input/AuraInputConfig.h
@@ -0,0 +1,36 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AuraInputConfig.generated.h"
+
+class UInputAction;
+
+USTRUCT(BlueprintType)
+struct FAuraInputAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	UInputAction* InputAction;
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UAuraInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag,bool bLogNotFound = false);
+protected:
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly)
+	TArray<FAuraInputAction> AbilityInputActions;
+};

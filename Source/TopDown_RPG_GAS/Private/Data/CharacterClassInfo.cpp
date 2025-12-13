8150ed02568c0c955c718aae2d0179db1e418e21
xiaofengzi <1679712850@qq.com>
2025-12-13 21:50:31 +0900

    Character Class Info

---
+++ Source/TopDown_RPG_GAS/Private/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright 13
+
+
+#include "Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}

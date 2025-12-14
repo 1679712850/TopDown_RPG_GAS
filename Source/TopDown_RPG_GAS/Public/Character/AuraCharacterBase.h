89e4d908062236387dd1bf49e3ce51882b198f02
xiaofengzi <1679712850@qq.com>
2025-12-14 13:45:27 +0900

    Dissolve Effect

--- Source/TopDown_RPG_GAS/Public/Character/AuraCharacterBase.h
+++ Source/TopDown_RPG_GAS/Public/Character/AuraCharacterBase.h
@@ -58,6 +58,21 @@ protected:
 	void AddCharacterAbilities();
 
 	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
+
+	/* Dissolve Effects */
+	void Dissolve();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartDissolveTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartWeaponDissolveTimeline(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UMaterialInstance> DissolveMaterialInstance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UMaterialInstance> WeaponDissolveMaterialInstance;
 private:
 	UPROPERTY(EditAnywhere, Category = "Abilities")
 	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;

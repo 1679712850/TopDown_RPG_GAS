9afd3cc1cf284d028a50ee86f82d48dd3ac53c6a
xiaofengzi <1679712850@qq.com>
2025-12-14 19:25:00 +0900

    Struct Ops Type Traits

--- Source/TopDown_RPG_GAS/Public/AuraAbilityTypes.h
+++ Source/TopDown_RPG_GAS/Public/AuraAbilityTypes.h
@@ -19,6 +19,18 @@ public:
 		return FAuraGameplayEffectContext::StaticStruct();
 	}
 
+	virtual FGameplayEffectContext* Duplicate() const override
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+
 	/** Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
 
@@ -28,4 +40,14 @@ protected:
 	
 	UPROPERTY()
 	bool bIsCriticalHit = false;
+};
+
+template<>
+struct TStructOpsTypeTraits< FAuraGameplayEffectContext > : public TStructOpsTypeTraitsBase2< FAuraGameplayEffectContext >
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true		// Necessary so that TSharedPtr<FHitResult> Data is copied around
+	};
 };

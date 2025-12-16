ce278ba27b1d64fb57652cd1db6c2c1aec3a1bae
xiaofengzi <1679712850@qq.com>
2025-12-16 21:51:52 +0900

    Behavior Tree Service

---
+++ Source/TopDown_RPG_GAS/Private/AI/Service/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright 13
+
+
+#include "AI/Service/BTService_FindNearestPlayer.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
---
+++ Source/TopDown_RPG_GAS/Public/AI/Service/BTService_FindNearestPlayer.h
@@ -0,0 +1,18 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_FindNearestPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType,Blueprintable)
+class TOPDOWN_RPG_GAS_API UBTService_FindNearestPlayer : public UBTService
+{
+	GENERATED_BODY()
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};

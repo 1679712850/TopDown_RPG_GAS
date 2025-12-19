16e68edb8e460730caf70b3f66dee6677817f8be
xiaofengzi <1679712850@qq.com>
2025-12-19 22:41:14 +0900

    Impact Effects

--- Source/TopDown_RPG_GAS/Public/Character/AuraCharacterBase.h
+++ Source/TopDown_RPG_GAS/Public/Character/AuraCharacterBase.h
@@ -8,6 +8,7 @@
 #include "Interaction/CombatInterface.h"
 #include "AuraCharacterBase.generated.h"
 
+class UNiagaraSystem;
 class UGameplayAbility;
 class UGameplayEffect;
 class UAttributeSet;
@@ -35,6 +36,7 @@ public:
 	virtual bool IsDead_Implementation() const override;
 	virtual AActor* GetAvatar_Implementation() override;
 	virtual TArray<FTaggedMontage> GetAttackMontages_Implementation() override;
+	virtual UNiagaraSystem* GetBloodEffect_Implementation() override;
 	/* end Combat Interface*/
 protected:
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Combat")
@@ -88,6 +90,9 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<UMaterialInstance> WeaponDissolveMaterialInstance;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	UNiagaraSystem* BloodEffect;
 private:
 	UPROPERTY(EditAnywhere, Category = "Abilities")
 	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;

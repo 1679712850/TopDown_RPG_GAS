176d4c97f3c402b72303a6f184009b259e99b20d
xiaofengzi <1679712850@qq.com>
2026-02-03 22:54:11 +0900

    Cooldown Async Task for UI cooldown display

---
+++ Source/TopDown_RPG_GAS/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,75 @@
+﻿// Copyright 13
+
+
+#include "AbilitySystem/AsyncTasks/WaitCooldownChange.h"
+
+#include "AbilitySystemComponent.h"
+
+UAbilityAsync_WaitCooldownChange* UAbilityAsync_WaitCooldownChange::WaitCooldownChange(
+	UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& CooldownTag)
+{
+	auto WaitCooldownChange = NewObject<UAbilityAsync_WaitCooldownChange>();
+	WaitCooldownChange->AbilitySystemComponent = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = CooldownTag;
+
+	if (!IsValid(AbilitySystemComponent) || !CooldownTag.IsValid())
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+	
+	// To know when a cooldown has ended (Cooldown Tag has been removed)
+	AbilitySystemComponent->RegisterGameplayTagEvent(
+		CooldownTag,
+		EGameplayTagEventType::NewOrRemoved).AddUObject(
+			WaitCooldownChange,
+			&UAbilityAsync_WaitCooldownChange::CooldownTagChanged);
+
+	// To know when a cooldown effect has been applied
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange, &UAbilityAsync_WaitCooldownChange::OnActiveEffectAdded);
+	
+	return WaitCooldownChange;
+}
+
+void UAbilityAsync_WaitCooldownChange::EndTask()
+{
+	if (!IsValid(AbilitySystemComponent)) return;
+	AbilitySystemComponent->RegisterGameplayTagEvent(CooldownTag, EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UAbilityAsync_WaitCooldownChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		CooldownEnd.Broadcast(0.f);
+	}
+}
+
+void UAbilityAsync_WaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC,
+	const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
+		TArray<float> TimesRemaining = AbilitySystemComponent->GetActiveEffectsTimeRemaining(GameplayEffectQuery);
+		if (TimesRemaining.Num() > 0)
+		{
+			float TimeRemaining = TimesRemaining[0];
+			for (int32 i = 0; i < TimesRemaining.Num(); i++)
+			{
+				TimeRemaining = TimesRemaining[i] > TimeRemaining ? TimesRemaining[i] : TimeRemaining;
+			}
+			
+			CooldownStart.Broadcast(TimeRemaining);
+		}
+	}
+}
---
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,42 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCooldownChange.generated.h"
+
+struct FActiveGameplayEffectHandle;
+struct FGameplayEffectSpec;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangeSignature, float, TimeRemaining);
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UAbilityAsync_WaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownStart;
+	
+	UPROPERTY(BlueprintAssignable)
+	FCooldownChangeSignature CooldownEnd;
+	
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UAbilityAsync_WaitCooldownChange* WaitCooldownChange(class UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& CooldownTag);
+	
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+protected:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	FGameplayTag CooldownTag;
+	
+	void CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount);
+	void OnActiveEffectAdded(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle);
+};

644d522011a08111b965eea4d9e630792b9684b2
xiaofengzi <1679712850@qq.com>
2026-02-04 15:00:53 +0900

    Level Up Info Data Asset

---
+++ Source/TopDown_RPG_GAS/Private/Data/LevelUpInfo.cpp
@@ -0,0 +1,24 @@
+﻿// Copyright 13
+
+
+#include "Data/LevelUpInfo.h"
+
+int32 ULevelUpInfoDataAsset::FindLevelForXP(int32 XP)
+{
+	int Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}	
+	}
+	return Level;
+}

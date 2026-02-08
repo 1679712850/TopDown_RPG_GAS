c5009bb90f96b016a233b2179cdfcf2e53d29510
xiaofengzi <1679712850@qq.com>
2026-02-08 14:50:48 +0900

    Debuff Parameters

--- Source/TopDown_RPG_GAS/Public/AbilitySystem/Ability/DamageGameplayAbility.h
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/Ability/DamageGameplayAbility.h
@@ -21,11 +21,20 @@ protected:
 	TSubclassOf<UGameplayEffect> DamageEffectClass;
 	
 	UPROPERTY(EditDefaultsOnly, Category = "Damage")
-	TMap<FGameplayTag, FScalableFloat> DamageTypes;
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FGameplayTag DamageType;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FScalableFloat Damage;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffChance = 20.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDamage = 5.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffFrequency = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	float DebuffDuration = 5.f;
 };

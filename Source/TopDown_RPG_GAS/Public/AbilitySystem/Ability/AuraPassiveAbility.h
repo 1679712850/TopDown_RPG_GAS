10cb753e80fccc85ee544d7f43ec07dd1384c479
xiaofengzi <1679712850@qq.com>
2026-02-09 21:29:14 +0900

    Aura Passive Ability

---
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/Ability/AuraPassiveAbility.h
@@ -0,0 +1,20 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraPassiveAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UAuraPassiveAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	void ReceiveDeactivate(const FGameplayTag& AbilityTag);
+};

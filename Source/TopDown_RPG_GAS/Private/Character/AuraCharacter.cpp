93438eab8985ce96356000887e51c424db84b744
xiaofengzi <1679712850@qq.com>
2026-02-09 22:54:55 +0900

    Wait Input Press

--- Source/TopDown_RPG_GAS/Private/Character/AuraCharacter.cpp
+++ Source/TopDown_RPG_GAS/Private/Character/AuraCharacter.cpp
@@ -140,6 +140,7 @@ void AAuraCharacter::ShowMagicCircle_Implementation(UMaterialInterface* DecalMat
 	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
 	{
 		AuraPlayerController->ShowMagicCircle(DecalMaterial);
+		AuraPlayerController->bShowMouseCursor = false;
 	}
 }
 
@@ -148,6 +149,7 @@ void AAuraCharacter::HideMagicCircle_Implementation()
 	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
 	{
 		AuraPlayerController->HideMagicCircle();
+		AuraPlayerController->bShowMouseCursor = true;
 	}
 }
 

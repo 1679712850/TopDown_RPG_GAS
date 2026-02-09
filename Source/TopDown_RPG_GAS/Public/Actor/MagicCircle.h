5c0224876563445de3fb8f3e6298825a1ac2ceb0
xiaofengzi <1679712850@qq.com>
2026-02-09 22:26:02 +0900

    Magic Circle

---
+++ Source/TopDown_RPG_GAS/Public/Actor/MagicCircle.h
@@ -0,0 +1,22 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+UCLASS()
+class TOPDOWN_RPG_GAS_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	AMagicCircle();
+	virtual void Tick(float DeltaTime) override;
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+};

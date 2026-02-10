0a38bdec2a29a27cd68b5d4de783adf8eb736358
xiaofengzi <1679712850@qq.com>
2026-02-10 20:38:41 +0900

    Spell Descriptions

---
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/Ability/ArcaneShards.h
@@ -0,0 +1,22 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageGameplayAbility.h"
+#include "ArcaneShards.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UArcaneShards : public UDamageGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 MaxNumShards = 11;
+};
---
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/Ability/Electrocute.h
@@ -0,0 +1,19 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraBeamSpell.h"
+#include "Electrocute.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UElectrocute : public UAuraBeamSpell
+{
+	GENERATED_BODY()
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+};

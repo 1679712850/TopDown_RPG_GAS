84013ea0c47d5f5456a612c4340d783e91580699
xiaofengzi <1679712850@qq.com>
2026-02-10 21:32:25 +0900

    Aura FireBall

---
+++ Source/TopDown_RPG_GAS/Private/Actor/AuraFireBall.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright 13
+
+
+#include "Actor/AuraFireBall.h"
+
+
+AAuraFireBall::AAuraFireBall()
+{
+}
+
+void AAuraFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraFireBall::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
---
+++ Source/TopDown_RPG_GAS/Public/Actor/AuraFireBall.h
@@ -0,0 +1,21 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraProjectile.h"
+#include "AuraFireBall.generated.h"
+
+UCLASS()
+class TOPDOWN_RPG_GAS_API AAuraFireBall : public AAuraProjectile
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAuraFireBall();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+};

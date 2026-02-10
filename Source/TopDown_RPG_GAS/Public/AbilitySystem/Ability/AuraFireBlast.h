05dcb3512a0b486e48624009a4140c19c2a30a3e
xiaofengzi <1679712850@qq.com>
2026-02-10 21:12:06 +0900

    FireBlast Ability

---
+++ Source/TopDown_RPG_GAS/Public/AbilitySystem/Ability/AuraFireBlast.h
@@ -0,0 +1,24 @@
+﻿// Copyright 13
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageGameplayAbility.h"
+#include "AuraFireBlast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_RPG_GAS_API UAuraFireBlast : public UDamageGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "FireBlast")
+	int32 NumFireBalls = 12;
+};
